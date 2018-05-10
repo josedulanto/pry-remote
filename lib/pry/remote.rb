6f76cc9305bbf0d0e8f8c54696792f8bc96c9241
Kevin Thompson <kevin@kevinthompson.info>
2018-05-10 14:56:06 -0700

    Extract IOUndumpedProxy Class

--- lib/pry/remote.rb
+++ lib/pry/remote.rb
@@ -5,71 +5,12 @@ require 'readline'
 require 'open3'
 
 require_relative './remote/input_proxy'
+require_relative './remote/io_undumped_proxy'
 
 class Pry
   module Remote
     DEFAULT_HOST = ENV['PRY_REMOTE_DEFAULT_HOST'] || '127.0.0.1'.freeze
     DEFAULT_PORT = ENV['PRY_REMOTE_DEFAULT_PORT'] || 9876
-
-    # Class used to wrap inputs so that they can be sent through DRb.
-    #
-    # This is to ensure the input is used locally and not reconstructed on the
-    # server by DRb.
-    class IOUndumpedProxy
-      include DRb::DRbUndumped
-
-      def initialize(obj)
-        @obj = obj
-      end
-
-      def completion_proc=(val)
-        if @obj.respond_to? :completion_proc=
-          @obj.completion_proc = proc { |*args, &block| val.call(*args, &block) }
-        end
-      end
-
-      def completion_proc
-        @obj.completion_proc if @obj.respond_to? :completion_proc
-      end
-
-      def readline(prompt)
-        if Readline == @obj
-          @obj.readline(prompt, true)
-        elsif @obj.method(:readline).arity == 1
-          @obj.readline(prompt)
-        else
-          $stdout.print prompt
-          @obj.readline
-        end
-      end
-
-      def puts(*lines)
-        @obj.puts(*lines)
-      end
-
-      def print(*objs)
-        @obj.print(*objs)
-      end
-
-      def printf(*args)
-        @obj.printf(*args)
-      end
-
-      def write(data)
-        @obj.write data
-      end
-
-      def <<(data)
-        @obj << data
-        self
-      end
-
-      # Some versions of Pry expect $stdout or its output objects to respond to
-      # this message.
-      def tty?
-        false
-      end
-    end
 
     # Ensure that system (shell command) output is redirected for remote session.
     System = proc do |output, cmd, _|
